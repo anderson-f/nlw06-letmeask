1858d66d3f13af64ac27a7186d4b613ad3a5d79c
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-08-11 17:52:28 -0300

    feat: authenticate user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import { auth, firebase } from '../services/firebase'
+import { useAuth } from "../hooks/useAuth";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -8,20 +8,18 @@ import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 
-import '../styles/auth.scss'
+import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
 
-  function handleCreateRoom() {
-    // history.push('/rooms/new')
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle()
+    }
 
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-    })
-
+    history.push("/rooms/new");
   }
 
   return (

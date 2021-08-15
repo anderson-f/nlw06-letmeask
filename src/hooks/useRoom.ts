e435658b30de66206569605dc90b483b4d505f44
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-08-15 15:23:05 -0300

    feat: admin room page and like question on user room page

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<
   string,
@@ -11,6 +12,9 @@ type FirebaseQuestions = Record<
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+      authorId: string;
+    }>
   }
 >;
 
@@ -23,9 +27,12 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 };
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth()
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
@@ -44,14 +51,20 @@ export function useRoom(roomId: string) {
             author: value.author,
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
           };
         }
       );
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
+
+      return () => {
+        roomRef.off('value')
+      }
     });
-  }, [roomId]);
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
